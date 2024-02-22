e821042adc0ba3371d10629550a20390275fa5ae
The Grey Walker <husky-on-line@hotmail.com>
2024-02-22 21:45:29 +0530

    Started fun thing ... Just starting

---
+++ JavaScript Fundamentals/main.js
@@ -0,0 +1,8 @@
+const ages = [21, 22, 23, 24, 25];
+const numberOfPeople = ages.length;
+
+for (let i = 0; i < numberOfPeople; i++) {
+    if (ages[i] % 2 == 0){
+        console.log(ages[i]);
+    }
+}
